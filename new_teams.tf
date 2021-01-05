4edf9ad973c246c3363ba5925cf43cd5ca7a6ba5
Marc-André BIRON <marcandre.biron@edifixio.com>
2021-01-05 18:59:23 +0100

    5th commit

--- new_teams.tf
+++ new_teams.tf
@@ -4,7 +4,7 @@ terraform {
       source  = "sysdiglabs/sysdig"
       version = ">= 0.4.0"
       sysdig_monitor_api_token = var.monitor_token
-      #sysdig_secure_api_token  = var.secure_token
+      sysdig_secure_api_token  = var.monitor_token
     }
   }
   resource "sysdig_secure_team" "sample" {
