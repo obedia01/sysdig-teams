baca4a8fc1e533ad2a6f762582740f6d460caf13
Marc-André BIRON <marcandre.biron@edifixio.com>
2021-01-06 00:00:29 +0100

    final tf 2

--- new_teams.tf
+++ new_teams.tf
@@ -1,15 +1,7 @@
-terraform {
-  required_providers {
-    sysdig = {
-      source  = "sysdiglabs/sysdig"
-      version = ">= 0.4.0"
-      sysdig_monitor_api_token = var.monitor_token
-      sysdig_secure_api_token  = var.monitor_token
-    }
+resource "sysdig_monitor_team" "sample" {
+name = "sample-team"
+entrypoint {
+    type = "Explore"
   }
-  resource "sysdig_secure_team" "sample" {
-  name               = "sample-team"
-  description        = "sample"
-  scope_by           = "container"
-  use_sysdig_capture = false}
+#use_sysdig_capture = false
 }
--- terraform.tfvars
+++
@@ -1,3 +0,0 @@
-# File: terraform.tfvars
-
-monitor_token = "e65b1d68-b324-4eca-8edd-109a070d15ec"
