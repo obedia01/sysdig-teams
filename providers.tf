50723c26aecae27efb75fe5429d34e63a871f354
Marc-André BIRON <marcandre.biron@edifixio.com>
2021-01-06 00:00:11 +0100

    final tf

---
+++ providers.tf
@@ -0,0 +1,5 @@
+provider "sysdig" {
+  sysdig_monitor_api_token = "e65b1d68-b324-4eca-8edd-109a070d15ec"
+  sysdig_monitor_url = "https://eu-de.monitoring.cloud.ibm.com"
+  sysdig_monitor_insecure_tls = true
+}
---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+# File: terraform.tfvars
+
+monitor_token = "e65b1d68-b324-4eca-8edd-109a070d15ec"
