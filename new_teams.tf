9a13e9ba3ae9c81c9dc0ea2fc6f20ca76ee4f915
Marc-André BIRON <marcandre.biron@edifixio.com>
2021-01-05 22:09:00 +0100

    Revert "7th commit"
    This reverts commit 1048876e16f88d9a74d32647976099b47b6f3ddd.

--- new_teams.tf
+++ new_teams.tf
@@ -3,8 +3,8 @@ terraform {
     sysdig = {
       source  = "sysdiglabs/sysdig"
       version = ">= 0.4.0"
-      sysdig_monitor_api_token = "e65b1d68-b324-4eca-8edd-109a070d15ec"
-      sysdig_secure_api_token  = "e65b1d68-b324-4eca-8edd-109a070d15ec"
+      sysdig_monitor_api_token = var.monitor_token
+      sysdig_secure_api_token  = var.monitor_token
     }
   }
   resource "sysdig_secure_team" "sample" {
